f9eb2d7b4e1d412dda4c5cfa458c81cf7fbacf38
Aaditya <aaditya2110s@gmail.com>
2026-04-04 12:12:35 +0530

    Shifting from react.fc

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-interface ProtectedRouteProps {
-  children: React.ReactNode;
+type Props = {
+  children : React.ReactNode
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+const ProtectedRoute = ({children}: Props)=> {
   const { token, isLoading } = useAuth();
 
   if (isLoading) {
