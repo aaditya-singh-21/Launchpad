d4332f3ba80116e71a7bda96a76e4f192a053aa1
Aaditya <aaditya2110s@gmail.com>
2026-04-08 23:11:14 +0530

    Changing origin with frontend deployment

--- backend/index.ts
+++ backend/index.ts
@@ -11,7 +11,7 @@ const PORT = Number(process.env.PORT) || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
-  origin: '*',
+  origin: 'https://launchpad-kappa-nine.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
